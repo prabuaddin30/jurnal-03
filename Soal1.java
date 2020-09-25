324753c3aa57bdef6a525960d4ace73e1af6dcdd
prabuaddin30 <prabuaddin@student.telkomuniversity.ac.id>
2020-09-25 08:08:54 +0700

    Soal1.java

---
+++ Soal1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Soal1{
+   public static void main (String[] args){
+   int bilangan, kuadrat;
+   Scanner input = new Scanner(System.in);
+   bilangan = 4;
+   kuadrat = bilangan*bilangan;
+   System.out.println("Kuadrat "+bilangan+" ="+kuadrat);
+   }
+}
