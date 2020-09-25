4fdadc345598bc27fc0367a023d4c194fb32de6c
prabuaddin30 <prabuaddin@student.telkomuniversity.ac.id>
2020-09-25 08:11:59 +0700

    Soal2.java

---
+++ Soal2.java
@@ -0,0 +1,11 @@
+public class Soal2 {
+    public static void main(String[] args){
+        String mataKuliah;
+        int jmlJam;
+        mataKuliah = "Implementasi Algoritma";
+        jmlJam = 6;
+        System.out.println("Aku suka kuliah "+mataKuliah);
+        System.out.println("Kuliah "+jmlJam+" jam perminggu rasanya kurang");
+        System.out.println("Tambahin donk jadi "+(jmlJam*2)+" perminggu :) ");
+    }
+}
